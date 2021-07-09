a8dd68308353b09e1bc320b60b42b5dbd44bf307
kmi0817 <62174395+kmi0817@users.noreply.github.com>
2021-07-09 18:24:42 +0900

    Update index.js
    make clean...

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 
 import fs from 'fs';
-import { getRecord } from './controllers/server.js';
+import { getRecords, getRecord, postRecord, updateRecord, patchRecord, deleteRecord } from './controllers/server.js';
 import moviesRoutes from './routes/movies.js';
 
 
@@ -16,84 +16,21 @@ server.use(bodyParser.json()); // meaning we will use JSON data in our whole ser
 
 server.use('/movies', moviesRoutes);
 
-server.get('/', (req, res) => {
-  fs.readdir('Ghibli', (err, movies) => {
-    const list = makeList(movies);
-    const body = `
-    <form action='/create_process' method='POST'>
-      <p><input type='text' name='title' placeholder='title'></>
-      <p><input type='text' name='director' placeholder='director'></>
-      <p><input type='text' name='release_date' placeholder='release_date'></>
-      <p><textarea name='description' placeholder='내용'></textarea></>
-      <p><input type='submit' value='제출'></p>
-    </form>
-    `;
-    const template = templateHTML(list, body);
-    res.send(template);
-  });
-});
 
-server.post('/create_process', (req, res) => {
-  const movie = req.body;
-  const id = uuidv4();
+server.get('/', getRecords);
 
-  const movie_with_id = {id: id, ...movie}
-  const json = JSON.stringify(movie_with_id, null, 2); // 이거 안 하면 파일 저장 시 Object로만 저장됨
+server.get('/:id', getRecord);
 
-  fs.writeFile(`Ghibli/${id}`, `${json}`, 'utf8', (err) => {
-    if (err) throw err;
-    console.log("file saved");
-  });
-  res.send(json);
-});
+server.post('/create_process', postRecord);
 
-server.get('/:id', (req, res) => {
-  const id = req.params.id;
+server.get('/update/:id', updateRecord);
 
-  fs.readdir('Ghibli', (err, movie) => {
-    const list = makeList(movies);
+server.post('/update_process', patchRecord);
 
-    fs.readFile(`Ghibli/${id}`, 'utf8', (err, movie) => {
-      const body = `
-      <div>${movie}</div>
-      `;
-
-      const template = templateHTML(list, body);
-      res.send(template);
-    });
-  });
-});
-
+server.post('/delete', deleteRecord);
 
 server.delete('/delete/:id')
+
 server.listen(PORT, () => {
   console.log(`The server is running on ${PORT}`);
-})
-
-function templateHTML(list, body) {
-  return `
-    <!doctype HTML>
-    <html>
-    <head>
-      <title>Ghibli</title>
-      <meta charset='utf-8'>
-    </head>
-    <body>
-      <h1><a href='/'>index</a></h1>
-      <a href='/delete'>delete</a>
-      ${list}
-      ${body}
-    </body>
-    <html>
-  `;
-}
-
-function makeList(filelist) {
-  var list = '<ul>';
-  filelist.forEach((file) => {
-    list += `<li><a href='${file}'>${file}</a></li>`;
-  });
-  list += '</ul>';
-
-  return list;
-}
+});
